2a6ab2560d2bc1ddf758a169837f1b3c8b9a5fe3
Sirjaime007 <93615006+Sirjaime007@users.noreply.github.com>
2026-09-05 20:42:51 -0300

    AAAAAAAAAAAAAA

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import lotus from '@prosefly/astro-theme-lotus';
-import themeConfig from './src/theme.config';
+import themeConfig from './src/theme.config.ts';
 
 export default defineConfig({
   integrations: [lotus(themeConfig)],
