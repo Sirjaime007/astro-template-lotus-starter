699c31ae9257b69b70ff72fac66bc08326cedbfa
Sirjaime007 <93615006+Sirjaime007@users.noreply.github.com>
2026-09-05 20:24:25 -0300

    aaaaaaa

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,11 @@
+starlight({
+      title: 'Tu Título',
+      // AGREGA ESTAS TRES LÍNEAS:
+      components: {
+        Hero: './src/components/EmptyHero.astro',
+      },
+      // ... el resto de tus opciones siguen igual
+
 import { defineConfig } from 'astro/config';
 import lotus from '@prosefly/astro-theme-lotus';
 
