78ead8dbb99bb3df3fa610f0300d0a93743cacb3
Sirjaime007 <93615006+Sirjaime007@users.noreply.github.com>
2026-09-05 20:41:21 -0300

    ASDAFASDAS

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import lotus from '@prosefly/astro-theme-lotus';
+import themeConfig from './src/theme.config';
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Cofiteca x Mad Diseño',
-      components: {
-        Hero: './src/components/EmptyHero.astro',
-      },
-    }),
-  ],
+  integrations: [lotus(themeConfig)],
 });
